93753ba3677b276c4e4ba6db43872ec4fea15cc4
Jannatin Naim <soup@jannatinnaim.com>
2022-10-21 19:37:42 +0600

    MacOS / update github abbreviations

--- MacOS/zsh/.config/zsh/abbrs/gh.abbr.zsh
+++ MacOS/zsh/.config/zsh/abbrs/gh.abbr.zsh
@@ -1,3 +1,4 @@
 abbr gb="gh browse"
 abbr gpurc="gh repo create --public --source=. --remote=origin"
 abbr gprrc="gh repo create --private --source=. --remote=origin"
+abbr ghpr="gh pr create --fill"
