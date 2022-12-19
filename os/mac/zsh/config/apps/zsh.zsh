d20b35899dd92ea95e5fc51d414caa19de3da560
Jannaitn Naim <soup@jannatinnaim.com>
2022-12-19 12:36:40 +0600

    add tmux base config

--- os/mac/zsh/config/apps/zsh.zsh
+++ os/mac/zsh/config/apps/zsh.zsh
@@ -10,3 +10,5 @@ zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!
 zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
 setopt globdots
+
+KEYTIMEOUT=10
--- scripts/install/mac.sh
+++ scripts/install/mac.sh
@@ -12,7 +12,7 @@ ln -sf "$MAC_DOTFILES/zsh/.zprofile" "$HOME"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # BREW PACKAGES
-brew install asdf zoxide exa
+brew install asdf zoxide exa tmux
 
 # ANTIDOTE INSTALLATION
 git clone --depth=1 https://github.com/mattmc3/antidote.git $HOME/.local/.antidote
@@ -21,3 +21,9 @@ git clone --depth=1 https://github.com/mattmc3/antidote.git $HOME/.local/.antido
 curl https://sdk.cloud.google.com > /tmp/gcloud-install.sh
 . /tmp/gcloud-install.sh --install-dir="$HOME/.gcloud"
 rm -rf /tmp/gcloud-install.sh
+
+# LINK TMUX CONFIG FILES
+ln -sf "$MAC_DOTFILES/tmux/.tmux.conf" "$HOME"
+
+# TMUX PLUGIN MANAGER INSTALLATION
+git clone https://github.com/tmux-plugins/tpm ~/.local/tmux/plugins/tpm
